7cdd53bc3a18f9e8d139f6b8950cbc302de7c667
Itiel Felix <Itiel Felix@FSociety>
2022-09-14 11:16:12 -0700

    Modificacion del Metodo crearMaterias

--- src/main/java/computacionfim/conversorhorarios/ControlConversor.java
+++ src/main/java/computacionfim/conversorhorarios/ControlConversor.java
@@ -110,14 +110,16 @@ public class ControlConversor {
         int contadorMateriasHoja = 1;
         guardarLineas(crearEncabezado());
         //guardarLineas(crearMateria());
-        System.out.println(materiaActual);
+        crearMateria(materiaActual);
+        //System.out.println(materiaActual);
 
         for (int i = 1; i < materias.size() - 1; i++) {
             numMateriaActual = i;
             materiaActual = materias.get(numMateriaActual);
 
             if (Integer.valueOf(materiaActual.getGrupo()) == grupoActual) {
-                System.out.println(materiaActual);
+                crearMateria(materiaActual);
+                //System.out.println(materiaActual);
                 //guardarLineas(crearMateria());
                 contadorMateriasHoja++;
                 if (contadorMateriasHoja > MaxMateriaHoja) {
@@ -126,7 +128,8 @@ public class ControlConversor {
                     guardarLineas(saltoDeHoja(saltos));
                     numPagActual++;
                     guardarLineas(crearEncabezado());
-                    System.out.println(materiaActual);
+                    crearMateria(materiaActual);
+                    //System.out.println(materiaActual);
                     //guardarLineas(crearMateria());
                     contadorMateriasHoja = 1;
                 }
@@ -140,7 +143,8 @@ public class ControlConversor {
                 numCarreraActual = Integer.valueOf(materiaActual.getNumeroPE());
                 contadorMateriasHoja = 1;
                 crearEncabezado();
-                System.out.println(materiaActual);
+                crearMateria(materiaActual);
+                //System.out.println(materiaActual);
             }
         }
     }
@@ -192,59 +196,84 @@ public class ControlConversor {
         return encabezado;
     }
 
-    public void crearMateria() {
-        ArrayList<String> horas = new ArrayList<>();
-        horas.add("07:00 - 18:00");
-        horas.add("07:00 - 18:00");
-        horas.add("07:00 - 18:00");
-        horas.add("07:00 - 18:00");
-        horas.add("07:00 - 18:00");
-        horas.add("07:00 - 18:00");
-        horas.add("07:00 - 18:00");
-        for (int i = 0; i < horas.size(); i++) {
-            horas.set(i, acondicionarHoras(horas.get(i)));
+        public String[] crearMateria(Materia materia){
+         ArrayList<String> horas = new ArrayList<>();
+        horas.add(materia.getLunes());
+        horas.add(materia.getMartes());
+        horas.add(materia.getMiercoles());
+        horas.add(materia.getJueves());
+        horas.add(materia.getViernes());
+        horas.add(materia.getSabado());
+        horas.add(materia.getDomingo());
+        for(int i = 0; i<horas.size();i++){
+            horas.set(i,acondicionarHoras(horas.get(i)));
         }
-        int numPE = 33537;
-        int claveAsig = 339;
-        int numEmpleado = 031054;
-        String nombreMateria = "Ecuaciones diferenciales";
-        String nombreProfesor = "URIARTE DE LA CRUZ PABLO JESUS";
-        int edificio = 3;
-        int salon = 505;
-        int capacidad = 40;
-        char tipo = 'C';
-        String subgrupo = "1";
-        String[] textoMateria = new String[3];
-        textoMateria[0] = espacios(69) + "|" + espacios(7) + "|" + espacios(7) + "|" + espacios(7) + "|" + espacios(7) + "|" + espacios(7) + "|" + espacios(7) + "|" + espacios(7) + "|\n";
-        textoMateria[1] = espacios(6) + numPE + espacios(2) + nombreMateria;
-        while (textoMateria[1].length() < 40) {
-            textoMateria[1] += " ";
+        String claveAsig = materia.getClaveUA();
+        while(claveAsig.length()<5){
+            claveAsig+=" ";
         }
-        textoMateria[1] = capacidad + espacios(2) + tipo + espacios(5) + "| " + horas.get(0).split("-")[0] + " | " + horas.get(1).split("-")[0] + " | " + horas.get(2).split("-")[0] + " | " + horas.get(3).split("-")[0] + " | " + horas.get(4).split("-")[0] + " | " + horas.get(5).split("-")[0] + " | " + horas.get(6).split("-")[0] + " | ";
-        textoMateria[2] = " " + claveAsig + espacios(5) + numEmpleado + " " + nombreProfesor.substring(0, 29) + " " + edificio + espacios(4) + salon + espacios(13) + "| " + horas.get(0).split("-")[1] + " | " + horas.get(1).split("-")[1] + " | " + horas.get(2).split("-")[1] + " | " + horas.get(3).split("-")[1] + " | " + horas.get(4).split("-")[1] + " | " + horas.get(5).split("-")[1] + " | " + horas.get(6).split("-")[1] + " | ";
+        String numeroControl = materia.getNumeroControl();
+        while(numeroControl.length()<4) {
+            numeroControl+=" ";
+        }
+        String es =materia.getExtra();
+        String numEmpleado = materia.getNumeroEmpleado();
+        String cero ="";
+        String numEmpleado2 = numEmpleado;
+        while(numEmpleado.length()<6){
+            cero+="0";
+            numEmpleado=cero+numEmpleado2;
+        }
+        String nombreMateria = materia.getNombreUA();
+        while(nombreMateria.length()<43) nombreMateria+=" ";
+        
+        String nombreProfesor=  materia.getNombreEmpleado();
+        while(nombreProfesor.length()<31) nombreProfesor+=" ";
+        
+        String edificio = materia.getEdificio();
+        String salon = materia.getSalon();
+        String capacidad=materia.getCapacidad();
+        if(capacidad.length()<2) capacidad+=" ";
+        String tipo = materia.getTipo();
+        String subgrupo =materia.getSubGrupo();
+        if(subgrupo.equals("")) subgrupo = " ";
+        String [] textoMateria = new String[3];
+        textoMateria[0] = espacios(69)+"|"+espacios(7)+"|"+espacios(7)+"|"+espacios(7)+"|"+espacios(7)+"|"+espacios(7)+"|"+espacios(7)+"|"+espacios(7)+"|";
+        textoMateria[1] =espacios(6)+claveAsig+espacios(2)+nombreMateria.substring(0,30)+" ";
+        while(textoMateria[1].length()<59){
+            textoMateria[1]+=" ";
+        }
+        textoMateria[1]+= capacidad+espacios(2)+tipo+espacios(5)+"| "+horas.get(0).split("-")[0]+" | "+horas.get(1).split("-")[0]+" | "+horas.get(2).split("-")[0]+" | "+horas.get(3).split("-")[0]+" | "+horas.get(4).split("-")[0]+" | "+horas.get(5).split("-")[0]+" | "+horas.get(6).split("-")[0]+" | ";
+
+        textoMateria[2] = " "+numeroControl+espacios(4)+numEmpleado+" "+nombreProfesor.substring(0,30)+"  "+edificio+espacios(4)+salon +espacios(7)+ subgrupo+espacios(5) + "| "+horas.get(0).split("-")[1]+" | "+horas.get(1).split("-")[1]+" | "+horas.get(2).split("-")[1]+" | "+horas.get(3).split("-")[1]+" | "+horas.get(4).split("-")[1]+" | "+horas.get(5).split("-")[1]+" | "+horas.get(6).split("-")[1]+" |  "+es;
         System.out.println(textoMateria[0]);
         System.out.println(textoMateria[1]);
         System.out.println(textoMateria[2]);
         System.out.println(linea());
+        return textoMateria;
     }
-
-    public String acondicionarHoras(String hora) {
-        String primeraHora = hora.split("-")[0].trim();
+    public String acondicionarHoras(String hora){
+        String horaGeneral;
+        if(!hora.equals("") && !hora.equals("     ")){
+        String primeraHora =hora.split("-")[0].trim();
         String segundaHora = hora.split("-")[1].trim();
-        if (primeraHora.charAt(0) == '0') {
-            primeraHora = " " + primeraHora.substring(1, 4);
+        if(primeraHora.charAt(0)=='0'){
+            primeraHora=" "+primeraHora.substring(1,5);
         }
-        if (segundaHora.charAt(0) == '0') {
-            segundaHora = " " + segundaHora.substring(1, 4);
+        if(segundaHora.charAt(0)=='0'){
+            segundaHora=" "+segundaHora.substring(1,5);
+    }
+        horaGeneral = primeraHora+"-"+segundaHora;
+        }else{
+            horaGeneral = espacios(5)+"-"+espacios(5);
         }
-        String horaGeneral = primeraHora + "-" + segundaHora;
         return horaGeneral;
-
+        
+    }
+    public String acondicionarNombre(String nombre){
+        return nombre.substring(0,30);
     }
 
-    public String acondicionarNombre(String nombre) {
-        return nombre.substring(0, 29);
-    }
 
     //Escribir una linea punteada en todo el renglon
     public String linea() {
